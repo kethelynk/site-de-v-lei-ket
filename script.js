3ae34f672833a425199cd4a2ed2b64961812ca32
kethelyn <133883348+kethelynk@users.noreply.github.com>
2025-05-26 11:37:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Função para validar o formulário
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulário enviado com sucesso!');
+});
